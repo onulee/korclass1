a84bbd20fe0b4d44721ae83af4985a35a9a8b507
005 <005@DESKTOP-9H7317A>
2023-05-25 10:55:44 +0900

    5월 25일 수업

--- j0524/src/j0524/C0524_03.java
+++ j0524/src/j0524/C0524_03.java
@@ -18,14 +18,14 @@ public class C0524_03 {
 		f.water();
 		//c2.siren(); 부모에는 siren() 주소가 없슴.
 		//c3.water(); 부모에는 water() 주소가 없슴.
-		aa = (Ambulance)c2;
+		aa = (Ambulance)c2;  //Ambulance aa = new Ambulance();
 		aa.siren();
 		
 		// 다른자손의 형변환은 안됨.
 		//aa = (Ambulance)c3;
 		
 		// 자손의 참조변수로 부모의 객체를 다루는 것은 안됨.
-		//aa = (Ambulance)c;
+		//aa = (Ambulance)c;  // Ambulance aa = new Car();
 		//Ambulance aa = new Car();
 		//aa.siren();
 		
