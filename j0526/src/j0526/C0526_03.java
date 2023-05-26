c2f3410e86757d8f86a1795d406cef240d374304
005 <005@DESKTOP-9H7317A>
2023-05-26 18:20:29 +0900

    5월 26일 수업 lang패키지

---
+++ j0526/src/j0526/C0526_03.java
@@ -0,0 +1,24 @@
+package j0526;
+
+import java.io.FileWriter;
+
+public class C0526_03 {
+
+	public static void main(String[] args) throws Exception {
+		FileWriter fw = null; //문자를 저장할수 있는 객체생성
+		fw = new FileWriter("c:/data/stu.txt");
+		String[] name = {"홍길동","유관순","이순신","강감찬","김구"};
+		String str = "";
+		for(int i=0;i<5;i++) {
+			str = str + (1001+i)+","+name[i]+",100,100,99 \r\n";
+		}
+		fw.write(str);
+		fw.flush();
+		fw.close();
+		
+		System.out.println("파일이 저장되었습니다.");
+		
+
+	}
+
+}
---
+++ j0526/src/j0526/C0526_12.java
@@ -0,0 +1,31 @@
+package j0526;
+
+import java.util.Scanner;
+
+public class C0526_12 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String lotto1 = "1";
+		//String lotto2 = "1조12587";
+		// 1-9조 00000-99999
+		// 0-9 랜덤숫자를 맞추는 게임을 구현하시오.
+		
+		int random = (int)(Math.random()*10);
+		//System.out.println(random);
+		System.out.println("조의 앞자리 숫자를 입력하세요.");
+		String strNum = scan.next(); //1조12587
+		//String strNum2 = strNum.substring(0,1);
+		int myNum = Integer.parseInt(strNum.substring(0,1)); // 입력받은 문자열 1을 숫자1로 변경
+		
+		if(random==myNum) {
+			System.out.println("당첨");
+		}else {
+			System.out.println("꽝");
+		}
+		
+		
+
+	}
+
+}
