a3a372855f336681d05297f6a33e77ad4eeaaf62
005 <005@DESKTOP-9H7317A>
2023-05-31 18:22:14 +0900

    5월31일 학생성적프로그램

--- j0531/src/j0531/C0531_02.java
+++ j0531/src/j0531/C0531_02.java
@@ -9,11 +9,11 @@ public class C0531_02 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int choice = 0;
-		int stuCount = 3; //학번
+		//객체선언
+		StuProcess sp = new StuProcess();
 		List<Student> list = new ArrayList<>();
-		list.add(new Student(1,"홍길동",100,100,99));
-		list.add(new Student(2,"유관순",99,99,98));
+		int choice = 0;
+		int stuCount = list.size()+1; //학번
 		
 		while(true) {
 			System.out.println("[ 학생성적처리 프로그램 ]");
@@ -21,31 +21,41 @@ public class C0531_02 {
 			System.out.println("2. 학생출력");
 			System.out.println("8. 파일에서 가져오기");
 			System.out.println("9. 파일저장하기");
-			
 			System.out.println("-----------------------");
 			System.out.println("원하는 번호를 입력하세요.>>");
 			choice = scan.nextInt();
 			
 			switch(choice) {
 			case 1:
-				//객체선언
-				StuProcess sp = new StuProcess();
 				//메소드호출후 데이터전달 받음.
+				stuCount = list.size()+1;
 				Map<String,Object> map = sp.stuInput(list,stuCount);
 				list = (List<Student>) map.get("list");
 				stuCount = (int) map.get("stuCount");
+				break;
 				
-				//출력
-				for(int i=0;i<list.size();i++) {
-					System.out.printf("%d %s \n",list.get(i).getStuNo(),
-							list.get(i).getName());
-				}
+			case 2:
+				//메소드호출(매개변수 전달)-list
+				list = sp.stuOutput(list);
+				break;
+				
+			case 8: //파일에서 가져오기
+				list = sp.stuRead();
+				System.out.println("[ 파일 읽어오기가 완료되었습니다. ]");
+				System.out.println();
+				break;
+			
+			case 9:
+				
 				
 				break;
-			}
+				
+				
+				
+			}//switch
 			
-		}
+		}//while
 
-	}
+	}//main
 
-}
+}//class
